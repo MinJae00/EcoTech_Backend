107a30724bea9148efa614368a63f11e48e8a443
MinJae00 <exceedmin@gmail.com>
2023-07-20 23:25:16 +0900

    check post

--- routes/user/check/index.js
+++ routes/user/check/index.js
@@ -3,6 +3,6 @@ const message = require('../../../utils/message')
 
 const recordController = require('../../../controllers/eco_result')
 
-router.get('/', recordController.save_check)
+router.post('/', recordController.save_check)
 
 module.exports = router
